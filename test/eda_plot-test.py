f7cb69c20c36139090597344934e8f1a54eb1161
Johnson Leung <johnsonsyleung@gmail.com>
2025-12-11 15:33:42 -0800

    Update eda_plot-test.py

--- test/eda_plot-test.py
+++ test/eda_plot-test.py
@@ -1,13 +1,6 @@
-import click
 from pathlib import Path
 
-@click.command()
-@click.argument("figure_path", type=click.Path(exists=True))
-
 def test(figure_path):
-    path = Path(figure_path)
+    path = Path('/figures')
     files = [file for file in path.iterdir() if file.is_file()]
     assert len(files) == 3
-
-if __name__ == "__main__":
-    test()
